4af45c8e28d9171e21d3f759de16b778811a9830
ShiraZaltsman <shira.zaltsman@gmail.com>
2019-09-12 09:12:46 -0700

    fixed

--- sizeof.c
+++ sizeof.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
+
 #define SIZEOF(object) (char *)(&object+1) - (char *)(&object)
-#define DECLARE_TYPE(type) __typeof__(type) newvar
+#define DECLARE_TYPE(type) ((char*)(((type*)770) + 1) - (char*)770) 
+
 int main()
 {
 	int i;
 	printf("int var size: %lu\n", SIZEOF(i));
+
 	double x;
 	printf("double var size: %lu\n", SIZEOF(x));
+
 	int arr[14];
 	printf("int arr[14] size : %lu\n", SIZEOF(arr));
 
 	printf("double type size: %lu\n", sizeof(double));
-	DECLARE_TYPE(double);
-	printf("double type size: %lu\n", SIZEOF(newvar));
+
+	
+	printf("double type size: %lu\n", DECLARE_TYPE(double));
+
+	printf("size_t type size: %lu\n", DECLARE_TYPE(size_t));
 	
  
     return 0;
